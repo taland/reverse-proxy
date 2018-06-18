d16a84a855307014711a05a79d5c63bcf23601e5
taland <taland@syneforge.com>
2018-06-18 16:49:21 +0300

    Adds some improvements to VGO build

---
+++ proxy/proxy_test.go
@@ -0,0 +1,13 @@
+package proxy
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseQueryString(t *testing.T) {
+	assert.NoError(t, nil)
+	assert.True(t, true)
+	assert.Equal(t, "foo", "foo")
+}
